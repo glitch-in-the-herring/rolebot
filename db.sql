16df68798797d3519291d3587d516a0159459de6
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-04-09 05:48:18 +0700

    semicolons

--- db.sql
+++ db.sql
@@ -2,10 +2,10 @@ CREATE TABLE alias (
 	alias TEXT,
 	role_id INTEGER NOT NULL,
 	PRIMARY KEY(alias)
-)
+);
 
 CREATE TABLE category (
 	role_id INTEGER NOT NULL,
 	category TEXT,
 	PRIMARY KEY(role_id)
-)
+);
